416b0faf43466cdab58b3325add97188a8ddf293
fernando2dias <fernando3dias@gmail.com>
2022-05-17 23:44:25 -0300

    Context Created

---
+++ src/Api.Domain/Entities/UserEntity.cs
@@ -0,0 +1,12 @@
+namespace Api.Domain.Entities
+{
+    public class UserEntity : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+
+
+    }
+}
